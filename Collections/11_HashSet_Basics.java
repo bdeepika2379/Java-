e5dc57db81508769d59d376f4916d0e1785d51e9
bdeepika2379 <bdeepika2379@gmail.com>
2026-09-08 13:04:12 +0530

    Create 11_HashSet_Basics.java

---
+++ Collections/11_HashSet_Basics.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class HashSetBasics {
+    public static void main(String[] args) {
+
+        HashSet<Integer> numbers = new HashSet<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+
+        System.out.println("Elements in HashSet:");
+        System.out.println(numbers);
+    }
+}
